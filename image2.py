3ad6a98b2026220d0bc47fbcc54a13fc1f702689
Phuoc-ho206 <phlminh666@gmail.com>
2026-01-25 19:49:50 +0700

    Update image2.py

--- image2.py
+++ image2.py
@@ -36,3 +36,27 @@ dilated_image = cv2.dilate(canny_image, kernel, iterations=1)  # Dilation
 # cv2.imshow("dilated_image",dilated_image)
 
 ###########################################
+
+###### Draw contour and filter out the license plate  #############
+contours, hierarchy = cv2.findContours(dilated_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]  # Lấy 10 contours có diện tích lớn nhất
+# cv2.drawContours(img, contours, -1, (255, 0, 255), 3) # Vẽ tất cả các ctour trong hình lớn
+
+screenCnt = []
+for c in contours:
+    peri = cv2.arcLength(c, True)  # Tính chu vi
+    approx = cv2.approxPolyDP(c, 0.06 * peri, True)  # làm xấp xỉ đa giác, chỉ giữ contour có 4 cạnh
+    [x, y, w, h] = cv2.boundingRect(approx.copy())
+    ratio = w / h
+    # cv2.putText(img, str(len(approx.copy())), (x,y),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 3)
+    # cv2.putText(img, str(ratio), (x,y),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 3)
+    if (len(approx) == 4):
+        screenCnt.append(approx)
+
+        cv2.putText(img, str(len(approx.copy())), (x, y), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 3)
+
+if screenCnt is None:
+    detected = 0
+    print("No plate detected")
+else:
+    detected = 1
